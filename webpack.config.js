bb61fce97ea609f0823f59c71d3b6ba8369af500
Vladimir <vladimir.vkv@gmail.com>
2018-07-28 16:15:53 +0300

    Setup environment

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist')
+};
+
+const config = {
+  context: paths.src,
+  entry: {
+    app: './index'
+  },
+  output: {
+    path: paths.dist,
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
+
+module.exports = config;
